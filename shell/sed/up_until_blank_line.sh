722e3088fc17accd73b2b81eb2f949616ce6a03c
Gareth Field <field.gareth@gmail.com>
2023-01-18 11:39:21 -0500

    sans comments

--- shell/sed/up_until_blank_line.sh
+++ shell/sed/up_until_blank_line.sh
@@ -3,12 +3,6 @@ up_until_blank_line() {
     then
         return
     else
-        # sed 's/^\s*$/moo/' $1
-        # grep -n '^\s*$' $1 # -> 4:
-        # ohh, but if you just use cut what if it's line 157
-        # mmkay, so grep, write in a return if none found,
-        # then sed the grep for the colon, then del past the
-        # match, then sub out any whitespace
         blank_line=$(grep -n '^\s*$' $1 | sed 's/://')
         line_after=$(($(($blank_line)) + 1)) 
         sed "$line_after,\$d" $1 | sed 's/^\s*$//'
